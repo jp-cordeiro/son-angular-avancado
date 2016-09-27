cae4034337a996d9df8e9b87733f3636553eff05
Joao Paulo <jpc.araujo@hotmail.com>
2016-09-26 21:18:59 -0300

    Servidor php

--- source/server/post.php
+++ source/server/post.php
@@ -1,4 +1,5 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Content-Type, x-xsrf-token');
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
